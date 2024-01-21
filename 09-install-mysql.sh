a74105ced26c459120c0bbef30438cd8f53e51e5
sunil <you@example.com>
2024-01-21 22:27:47 +0530

    added 2nd script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -3,10 +3,11 @@
 ID=$(id -u)
 
 if [ $ID  -ne 0 ]
+then
 
-echo "ERROR:: Please run this script with root access"
+echo "ERROR:: Please run this script with root access" 
 
 else
-   echo "you are root user "
+    echo "you are root user "
 
    fi
