7ba2b13b22cd68c7488b729d3ebf5eafa56a35e2
sunil <you@example.com>
2024-01-21 22:12:16 +0530

    added 2nd script

--- 08-conditons.sh
+++ 08-conditons.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$1
 
-if [  $NUMBER -gt 100  ]
+if [ $NUMBER -gt 100 ]
 then
    echo "Give the number $NUMBER is greater than 100"
    else
     echo "Give the number $NUMBER is not greater than 100"
-    
+fi
