1c041e99da45a0f27678d11618d3887fc5428ab6
sunil <you@example.com>
2024-01-22 21:08:40 +0530

    added 2nd script

---
+++ 11-logs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "script name: $0"
+
+VALIDATE(){
+      if [ $1 -ne 0 ]
+     then
+         echo "ERROR:: $2----  failed"
+         exit 1
+
+    else
+       echo "$2------   SUCCESS"
+
+    fi
+}
+
+if [ $ID  -ne 0 ]
+then
+
+echo "ERROR:: Please run this script with root access" 
+     exit 1 # you can give  other than 0
+
+else
+    echo "you are root user "
+
+   fi
+
+    yum install mysql  -y
+
+
+    VALIDATE $? "INstalling MYSQL"
+
+    yum install git -y
+
+
+    VALIDATE $? "Installing git"
