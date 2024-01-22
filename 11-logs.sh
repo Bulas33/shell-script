4b4229cd6e6c2b91a9a25b16cf3fed7c5d75efb3
sunil <you@example.com>
2024-01-22 22:11:40 +0530

    added 2nd script

--- 11-logs.sh
+++ 11-logs.sh
@@ -6,14 +6,18 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 VALIDATE(){
       if [ $1 -ne 0 ]
      then
-         echo "ERROR:: $2----  failed"
+         echo -e "ERROR:: $2---- $R failed $N"
          exit 1
 
     else
-       echo "$2------   SUCCESS"
+       echo  -e "$2------  $G SUCCESS $N"
 
     fi
 }
@@ -21,7 +25,7 @@ VALIDATE(){
 if [ $ID  -ne 0 ]
 then
 
-echo "ERROR:: Please run this script with root access" 
+echo -e "$R ERROR:: Please run this script with root access $N" 
      exit 1 # you can give  other than 0
 
 else
