8b9d8b62079dbe955140d48e17b4b6bc0734ff33
sunil <you@example.com>
2024-01-28 20:55:29 +0530

    added 2nd script

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -1,48 +1,45 @@
 #!/bin/bash
 
 ID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-TIMESTAMP=$(date  +%F-%H-%M-%S)
-
+TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script started excuting at $TIMESTAMP" &>> $LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
-    if [$1 -ne 0]
+    if [ $1 -ne 0 ]
     then
-       echo -e "$2...$R FAILED $N"
+        echo -e "$2 ... $R FAILED $N"
     else
-     echo -e "$2...$G SUCCESS $N"
-    
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-if [ $ID  -ne 0 ]
+if [ $ID -ne 0 ]
 then
-
-echo -e "$R ERROR:: Please run this script with root access $N" 
-     exit 1 # you can give  other than 0
-
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
 else
-    echo "you are root user "
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 
-   fi
-    #echo "All arguments are passed: $@"
-    for package in $@
-    do
-         yum list installed $package &>> $LOGFILE                           
-         if [$? -ne 0]
-        then
-           yum install $package -y &>> $LOGFILE
-           VALIDATE $? "Installation of $package"
-           else
-              echo -e "$package is already installed ...$Y Skipping $N"
-              fi
+#echo "All arguments passed: $@"
+# git mysql postfix net-tools
+# package=git for first time
 
-             done
+for package in $@
+do
+    yum list installed $package &>> $LOGFILE #check installed or not
+    if [ $? -ne 0 ] #if not installed
+    then
+        yum install $package -y &>> $LOGFILE # install the package
+        VALIDATE $? "Installation of $package" # validate
+    else
+        echo -e "$package is already installed ... $Y SKIPPING $N"
+    fi
+done
